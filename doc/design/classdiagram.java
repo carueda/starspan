1ddc7166f3580237d40e5756ea8f6be8269de6f2
Carlos Rueda <carueda@gmail.com>
2004-08-23 05:49:21 +0000

    new design documentation

---
+++ doc/design/classdiagram.java
@@ -0,0 +1,23 @@
+/**
+ * @opt all
+ * @hidden
+ */
+class UMLOptions {}
+
+
+/** @tagvalue implicit */
+class Starspan {}
+
+class Raster {}
+class Vector {}
+
+/**
+  @depend - - - Observer 
+*/
+class Traverser {}
+
+interface Observer {}
+
+
+class EnviSlObserver implements Observer {}
+class DBObserver implements Observer {}
