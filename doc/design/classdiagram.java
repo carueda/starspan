3fbb90139397b9983597061353cc0bb5baf9d68c
Carlos Rueda <carueda@gmail.com>
2004-08-31 07:23:53 +0000

    update

--- doc/design/classdiagram.java
+++ doc/design/classdiagram.java
@@ -1,23 +1,60 @@
 /**
  * @opt all
+ * @opt types
  * @hidden
  */
 class UMLOptions {}
 
+/** @hidden */  class OGRLayer {}
+/** @hidden */  class GDALDataset {}
+/** @hidden */  class OGREnvelope {}
+/** @hidden */  class GDALDataType {}
+/** @hidden */  class Signature {}
+/** @hidden */  class DBFHandle {}
+/** @hidden */  class FILE {}
 
-/** @tagvalue implicit */
+
+/** 
+  * @stereotype implicit 
+  * @navassoc - <creates> - Raster 
+  * @navassoc - <creates> - Vector 
+  * @navassoc - <creates> - Traverser 
+  * @navassoc - <creates> - DBObserver 
+  * @navassoc - <creates> - EnviSlObserver 
+  */
 class Starspan {}
 
-class Raster {}
-class Vector {}
+class Raster {
+	public GDALDataset getDataset();
+}
+
+class Vector {
+	public OGRLayer getLayer(int layer_num);
+	public void report(FILE file);
+}
 
 /**
-  @depend - - - Observer 
-*/
-class Traverser {}
+  * @assoc - - - Observer 
+  * @depend - <uses> - Raster 
+  * @depend - <uses> - Vector 
+  */
+class Traverser {
+	public void setObserver(Observer aObserver);
+	public Observer getObserver();
+}
 
-interface Observer {}
+interface Observer {
+	public void intersection(int fid, OGREnvelope e);
+	public void addSignature(double x, double y, Signature s, GDALDataType t, int ts);
+}
 
 
-class EnviSlObserver implements Observer {}
-class DBObserver implements Observer {}
+class DBObserver implements Observer {
+	public DBFHandle file;
+}
+
+class EnviSlObserver implements Observer {
+	public FILE data_file;
+	public FILE header_file;
+}
+
