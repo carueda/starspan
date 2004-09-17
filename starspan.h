2d12f1683fa084adc5003d585be722b67e3f19f4
Carlos Rueda <carueda@gmail.com>
2004-09-17 04:21:49 +0000

    version 0.6beta

--- starspan.h
+++ starspan.h
@@ -14,7 +14,7 @@
 #include <stdio.h> // FILE
 
 
-#define STARSPAN_VERSION "0.5beta"
+#define STARSPAN_VERSION "0.6beta"
 
 
 /////////////////////////////////////////////////////////////////////////////
