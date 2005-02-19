9c84d8cf0e5ac6de4de450e2621400339b216454
Carlos Rueda <carueda@gmail.com>
2005-02-19 21:55:52 +0000

    cvs tag is conditioned by option -dotag

--- mksrcdist.sh
+++ mksrcdist.sh
@@ -3,10 +3,12 @@
 # $Id$
 #
 if [ -z $1 ]; then
-	echo "USAGE:  mksrcdist.sh <VERSION>"
+	echo "USAGE:  mksrcdist.sh <VERSION> [-dotag]"
+	echo "  -dotag:  make cvs -q tag release-<VERSION> before"
 	exit 1
 fi
 VERSION=$1
+dotag=$2
 if [ "$VERSION" != `cat VERSION` ]; then
 	echo "VERSION parameter must be equal to VERSION file contents"
 	exit 2
@@ -14,7 +16,7 @@ fi
 tag=`echo release-$VERSION | tr -t . _`
 cvsroot=":pserver:anonymous@cvs.casil.ucdavis.edu:/cvsroot/starspan"
 
-cvs -q tag $tag &&\
+(test "$dotag" != "-dotag" || cvs -q tag $tag) &&\
 mkdir -p DISTDIR &&\
 cd DISTDIR &&\
 cvs -d$cvsroot -q export -r $tag -d starspan-$VERSION starspan &&\
