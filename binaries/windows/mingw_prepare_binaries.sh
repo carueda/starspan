b952c79f4be997a1be3e01c42ee4197c0542e029
Carlos Rueda <carueda@gmail.com>
2008-07-26 21:57:19 +0000

    binaries/windows/mingw_prepare_binaries.sh

---
+++ binaries/windows/mingw_prepare_binaries.sh
@@ -0,0 +1,18 @@
+# Gets the required binaries and puts them in base_dist/bin/
+# Run this script under MinGW
+# $Id
+
+if [ "$OSTYPE" != "msys" ]; then
+    echo "Warning: are you sure this is the right machine?"
+fi
+
+# the prefix used to build GEOS, GDAL, and StarSpan on MinGW
+win_prefix=/usr/local
+
+# copy required binaries into base_dist/bin/
+cp -p ${win_prefix}/bin/libgeos_c-1.dll  base_dist/bin/
+cp -p ${win_prefix}/bin/libgeos-2.dll    base_dist/bin/
+cp -p ${win_prefix}/bin/libgdal-1.dll    base_dist/bin/
+cp -p ${win_prefix}/bin/ogrinfo.exe      base_dist/bin/
+cp -p ${win_prefix}/bin/gdalinfo.exe     base_dist/bin/
+cp -p ${win_prefix}/bin/starspan2.exe    base_dist/bin/
